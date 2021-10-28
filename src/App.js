553fc33667cce395f018e13ddbc26baf07272932
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2021-10-28 13:15:05 -0600

    Bohubrihi Resturent

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import MainComponents from "./components/MainComponents";
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+
+      <MainComponents />
+
+      
+    </div>
+  );
+}
+
+export default App;
